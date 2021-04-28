3d49f2b48b3172458216b9de6d1c230cf14154ed
jumbobumbo <dwheels91@gmail.com>
2021-04-28 20:09:41 +0100

    adafruit setup bash script

---
+++ install_sensor_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade setuptools
+cd ~
+sudo pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo python3 raspi-blinka.py
